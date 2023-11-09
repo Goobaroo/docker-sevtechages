ef0c6c518de331e12492773b3e79f63d63a08c1b
Jason McLeod <jason@mcleods.me>
2023-11-08 20:08:34 -0500

    Update to version 3.2.3

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# syntax=docker/dockerfile:1
+
+FROM openjdk:8u312-jre-buster
+
+LABEL version="3.2.3"
+
+RUN apt-get update && apt-get install -y curl unzip && \
+ adduser --uid 99 --gid 100 --home /data --disabled-password minecraft
+
+COPY launch.sh /launch.sh
+RUN chmod +x /launch.sh
+
+USER minecraft
+
+VOLUME /data
+WORKDIR /data
+
+EXPOSE 25565/tcp
+
+CMD ["/launch.sh"]
---
+++ launch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+
+cd /data
+
+if ! [[ "$EULA" = "false" ]] || grep -i true eula.txt; then
+	echo "eula=true" > eula.txt
+else
+	echo "You must accept the EULA by in the container settings."
+	exit 9
+fi
+
+if ! [[ -f SevTech_Ages_Server_3.2.3.zip ]]; then
+	rm -fr config journeymap mods resources scripts structures SevTech_Ages_Server*.zip
+	curl -Lo SevTech_Ages_Server_3.2.3.zip 'https://edge.forgecdn.net/files/3570/46/SevTech_Ages_Server_3.2.3.zip' && unzip -u -o SevTech_Ages_Server_3.2.3.zip -d /data
+	chmod +x Install.sh
+	./Install.sh
+fi
+
+if [[ -n "$MOTD" ]]; then
+    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+fi
+if [[ -n "$LEVEL" ]]; then
+    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+fi
+if [[ -n "$OPS" ]]; then
+    echo $OPS | awk -v RS=, '{print}' > ops.txt
+fi
+if [[ -n "$ALLOWLIST" ]]; then
+    echo $ALLOWLIST | awk -v RS=, '{print}' > white-list.txt
+fi
+
+. ./settings.sh
+JVM_OPTS = $JVM_OPTS $JAVA_PARAMETERS
+curl -Lo log4j2_112-116.xml https://launcher.mojang.com/v1/objects/02937d122c86ce73319ef9975b58896fc1b491d1/log4j2_112-116.xml
+java -server $JVM_OPTS -Dfml.queryResult=confirm -Dlog4j.configurationFile=log4j2_112-116.xml -jar $SERVER_JAR nogui
